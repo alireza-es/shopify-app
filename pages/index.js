8d7cd720a4b61b6213b41d8452dc2f386a675d1b
Alireza Esmaeili <alireza.es.dev@hotmail.com>
2020-11-12 18:24:32 +0330

    Build your user interface with Polaris

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
 const Index = () => (
-    <div>
-      <h1>Next.js Sample on react</h1>
-      <p>Sample app using React and Next.js</p>
-    </div>
-  );
-  
-  export default Index;
+  <Page>
+    <Layout>
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
+    </Layout>
+  </Page>
+);
+
+export default Index;

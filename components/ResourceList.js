5d0e97d14d2cfe60a7437605ab726dab7ac0c20c
Alireza Esmaeili <alireza.es.dev@hotmail.com>
2020-11-13 14:29:08 +0330

    Add product-edit mutation

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -9,6 +9,8 @@ import {
 } from '@shopify/polaris';
 import store from 'store-js';
 import React from "react";
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Context } from '@shopify/app-bridge-react';
 
 const GET_PRODUCTS_BY_ID = gql`
   query getProducts($ids: [ID!]!) {
@@ -39,7 +41,16 @@ const GET_PRODUCTS_BY_ID = gql`
   }
 `;
 class ResourceListWithProducts extends React.Component {
+    static contextType = Context;
     render() {
+        const app = this.context;
+        const redirectToProduct = () => {
+            const redirect = Redirect.create(app);
+            redirect.dispatch(
+                Redirect.Action.APP,
+                '/edit-products',
+            );
+        };
         const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
 
         return (
@@ -75,6 +86,10 @@ class ResourceListWithProducts extends React.Component {
                                             id={item.id}
                                             media={media}
                                             accessibilityLabel={`View details for ${item.title}`}
+                                            onClick={() => {
+                                                store.set('item', item);
+                                                redirectToProduct();
+                                              }}
                                         >
                                             <Stack>
                                                 <Stack.Item fill>

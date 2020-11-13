6696f2bef82eecbd48de9682a2cb58bffff4f064
Alireza Esmaeili <alireza.es.dev@hotmail.com>
2020-11-13 16:19:33 +0330

    Add subscription using billing API

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('koa-session');
 dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
+const getSubscriptionUrl = require('./server/getSubscriptionUrl');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -27,19 +28,19 @@ app.prepare().then(() => {
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: ['read_products', 'write_products'],
-      afterAuth(ctx) {
+      async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set('shopOrigin', shop, {
           httpOnly: false,
           secure: true,
           sameSite: 'none'
         });
-        ctx.redirect('/');
+        await getSubscriptionUrl(ctx, accessToken, shop);
       },
     }),
   );
 
-  server.use(graphQLProxy({version:ApiVersion.October19}));
+  server.use(graphQLProxy({ version: ApiVersion.October19 }));
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
